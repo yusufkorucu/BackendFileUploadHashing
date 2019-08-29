391c5e0456f2af20e39fe81fdcd550966535b965
yusufkorucu <32218432+yusufkorucu@users.noreply.github.com>
2019-08-29 08:56:43 +0300

    Add files via upload

---
+++ DenemeHash/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DenemeHash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileUpload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFileSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResponseTime = new System.Windows.Forms.TextBox();
+            this.txtHash = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFileUpload
+            // 
+            this.btnFileUpload.Location = new System.Drawing.Point(333, 86);
+            this.btnFileUpload.Name = "btnFileUpload";
+            this.btnFileUpload.Size = new System.Drawing.Size(227, 32);
+            this.btnFileUpload.TabIndex = 0;
+            this.btnFileUpload.Text = "FileUpload";
+            this.btnFileUpload.UseVisualStyleBackColor = true;
+            this.btnFileUpload.Click += new System.EventHandler(this.btnFileUpload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Size";
+            // 
+            // txtFileSize
+            // 
+            this.txtFileSize.Location = new System.Drawing.Point(237, 213);
+            this.txtFileSize.Name = "txtFileSize";
+            this.txtFileSize.Size = new System.Drawing.Size(501, 20);
+            this.txtFileSize.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "File Size    (Byte)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "File Size";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Response Time(MiliSecond)";
+            // 
+            // txtResponseTime
+            // 
+            this.txtResponseTime.Location = new System.Drawing.Point(237, 263);
+            this.txtResponseTime.Name = "txtResponseTime";
+            this.txtResponseTime.Size = new System.Drawing.Size(501, 20);
+            this.txtResponseTime.TabIndex = 2;
+            // 
+            // txtHash
+            // 
+            this.txtHash.Location = new System.Drawing.Point(237, 158);
+            this.txtHash.Name = "txtHash";
+            this.txtHash.Size = new System.Drawing.Size(501, 20);
+            this.txtHash.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Hash";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(192, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(473, 37);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "File Hash and Response Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 375);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtHash);
+            this.Controls.Add(this.txtResponseTime);
+            this.Controls.Add(this.txtFileSize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFileUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileUpload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFileSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResponseTime;
+        private System.Windows.Forms.TextBox txtHash;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DenemeHash/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DenemeHash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       /* private static string GetChecksum(string file)
+        {
+            using (System.IO.FileStream stream = File.OpenRead(file))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(stream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+            }
+        }
+        */
+        private static string GetChecksumBuffered(Stream stream)
+        {
+            using (var bufferedStream = new BufferedStream(stream, 1024 * 32))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(bufferedStream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+            }
+        }
+
+        private void btnFileUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            string FilePath = openFileDialog.FileName;
+            MessageBox.Show(FilePath);
+            string filesize = "";
+            long size = new System.IO.FileInfo(FilePath).Length;
+            filesize = size.ToString();
+            var stopwatch = new Stopwatch();
+            txtFileSize.Text = filesize;
+            stopwatch.Start();
+            //   string str = "";
+            stopwatch.Stop();
+            var stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            var fileStream = new System.IO.FileStream(FilePath, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Read);
+            string str2 = GetChecksumBuffered(fileStream);
+            txtHash.Text = str2;
+            long a = stopwatch2.ElapsedMilliseconds;
+            string responsetime = "";
+            responsetime = a.ToString();
+            txtResponseTime.Text = responsetime;
+        }
+    }
+}
